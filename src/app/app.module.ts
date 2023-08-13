72ebfca7e7fddb09703465ee7494d8e65ccf2d79
seaghan2000 <seaghan00@gmail.com>
2023-08-13 16:21:43 -0600

    Routing refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './parent/navbar/navbar.component';
-import { ParentComponent } from './parent/parent.component';
-import { HomeComponent } from './parent/home/home.component';
-import { MealOfTheDayComponent } from './parent/home/meal-of-the-day/meal-of-the-day.component';
-import { HomeMessageComponent } from './parent/home/home-message/home-message.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { MealOfTheDayComponent } from './home/meal-of-the-day/meal-of-the-day.component';
+import { HomeMessageComponent } from './home/home-message/home-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ParentComponent,
     HomeComponent,
     MealOfTheDayComponent,
     HomeMessageComponent
@@ -21,8 +19,7 @@ import { HomeMessageComponent } from './parent/home/home-message/home-message.co
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      {path: 'meals', component: MealOfTheDayComponent},
-      {path: 'home', component: ParentComponent},
+      {path: 'home', component: HomeComponent},
       {path: '', redirectTo: 'home', pathMatch: 'full'},
     ]),
   ],
