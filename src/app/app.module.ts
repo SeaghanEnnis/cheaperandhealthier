c88ec12c2eca1f1aa6898cf7ec1377dc5d5f9ab2
seaghan2000 <seaghan00@gmail.com>
2023-08-13 15:47:45 -0600

    SPA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './parent/navbar/navbar.component';
 import { ParentComponent } from './parent/parent.component';
 import { HomeComponent } from './parent/home/home.component';
 import { MealOfTheDayComponent } from './parent/home/meal-of-the-day/meal-of-the-day.component';
+import { HomeMessageComponent } from './parent/home/home-message/home-message.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,16 @@ import { MealOfTheDayComponent } from './parent/home/meal-of-the-day/meal-of-the
     NavbarComponent,
     ParentComponent,
     HomeComponent,
-    MealOfTheDayComponent
+    MealOfTheDayComponent,
+    HomeMessageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'meals', component: MealOfTheDayComponent},
+      {path: 'home', component: ParentComponent},
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { RouterModule, UrlSegment } from '@angular/router';
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
+
 export class ParentComponent {
 
 }
