99574aec92fd65abd9e878cf9077fd1d5e61fb57
seaghan2000 <seaghan00@gmail.com>
2023-09-09 11:26:45 -0600

    Hover colors

--- src/app/data/foods/spicies.ts
+++ src/app/data/foods/spicies.ts
@@ -1,6 +1,6 @@
 import {Food} from '../../models/food';
 
 export const SPICIES: Food[] = [
-    {id: 12, name: "Saffron",type: "Drink", price: 17.5, calories: 100},
-    {id: 13, name: "Grape Juice",type: "Drink", price: 1.5, calories: 89}
+    {id: 12, name: "Saffron",type: "Spice", price: 17.5, calories: 100},
+    {id: 13, name: "Grape Juice",type: "Spice", price: 1.5, calories: 89}
 ]
