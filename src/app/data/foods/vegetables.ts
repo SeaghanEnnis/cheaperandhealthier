0867b4cf618e7f13f01cc041df91d9e0466a97a4
seaghan2000 <seaghan00@gmail.com>
2023-09-25 19:35:50 -0600

    Squash

--- src/app/data/foods/vegetables.ts
+++ src/app/data/foods/vegetables.ts
@@ -2,5 +2,6 @@ import {Food} from '../../models/food';
 
 export const VEGETABLES: Food[] = [
     {id: 3, name: "Spinach",type: "Vegetable", price: 1.5, calories: 234},
-    {id: 4, name: "Zucchini",type: "Vegetable", price: 0.5, calories: 140}
+    {id: 4, name: "Zucchini",type: "Vegetable", price: 0.5, calories: 140},
+    {id: 18, name: "Squash",type: "Vegetable", price: 1.5, calories: 140}
 ]
