76c5b318cc577fc1684ea4d4c5977f712c212fe5
seaghan2000 <seaghan00@gmail.com>
2023-10-14 12:05:07 -0600

    Clean up

--- src/app/meals/meals.component.ts
+++ src/app/meals/meals.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
 import { MEALS } from '../data/meals/aMeal';
 import { Meal } from '../models/meal';
 
